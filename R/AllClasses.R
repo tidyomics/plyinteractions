f81612ba1de601c027589364ad7c91f57a21fd81
js2264 <jacquesserizay@gmail.com>
2023-10-08 22:54:58 +0200

    test: add validity functions for new classes

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -23,11 +23,35 @@ setClass("GroupedGInteractions",
     contains = c("DelegatingGInteractions")
 )
 
+setValidity("GroupedGInteractions",
+    function(object)
+    {
+        if (nrow(object@group_keys) != object@n)
+            return("`n` should be equal to the number of rows in `group_keys`")
+        if (length(object@group_indices) != length(object@delegate))
+            return("`group_indices` and GInteractions lengths should be identical")
+        if (any(!colnames(object@group_keys) %in% 
+            colnames(as_tibble(object@delegate)))
+        )
+            return("Some column names in `group_keys` are not found in the GInteractions object")
+        TRUE
+    }
+)
+
 setClass("PinnedGInteractions",
     slot = c(
         pin = "integer"
     ),
     contains = c("DelegatingGInteractions")
+)
+
+setValidity("PinnedGInteractions",
+    function(object)
+    {
+        if (!object@pin %in% c(1, 2))
+            return("`pin` should be either `1L` or `2L`")
+        TRUE
+    }
 )
 
 setClass("AnchoredPinnedGInteractions",
@@ -36,3 +60,13 @@ setClass("AnchoredPinnedGInteractions",
     ),
     contains = c("PinnedGInteractions")
 )
+
+setValidity("AnchoredPinnedGInteractions",
+    function(object)
+    {
+        if (!object@anchor %in% c(1, 2))
+            return("`anchor` should be one of 'start', 'end', 'center', '5p' or '3p'")
+        TRUE
+    }
+)
+
