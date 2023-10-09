f7e5888af19af26e130674e29ff8da4805e2116f
js2264 <jacquesserizay@gmail.com>
2023-10-09 14:43:22 +0200

    dox: hyperlinnks in package-level doc

--- R/plyinteractions.R
+++ R/plyinteractions.R
@@ -9,17 +9,17 @@
 #' verbs. These verbs can be used to construct human-readable analysis 
 #' pipelines based on `GInteractions`.
 #' 
-#'  - Group genomic interactions with `group_by`; 
-#'  - Summarize grouped genomic interactions with `summarize`; 
-#'  - Tally/count grouped genomic interactions with `tally` and `count`; 
-#'  - Modify genomic interactions with `mutate`; 
-#'  - Subset genomic interactions with `filter` using
+#'  - Group genomic interactions with \code{\link[=dplyr-group_by]{group_by}}; 
+#'  - Summarize grouped genomic interactions with \code{\link[=dplyr-summarize]{summarize}}; 
+#'  - Tally/count grouped genomic interactions with \code{\link[=dplyr-count]{tally}} and \code{\link[=dplyr-count]{count}}; 
+#'  - Modify genomic interactions with \code{\link[=dplyr-mutate]{mutate}}; 
+#'  - Subset genomic interactions with \code{\link[=dplyr-filter]{filter}} using
 #'  [`<data-masking>`](https://rlang.r-lib.org/reference/args_data_masking.html) 
 #'  and logical expressions; 
-#'  - Pick out any columns from the associated metadata with `select` 
+#'  - Pick out any columns from the associated metadata with \code{\link[=dplyr-select]{select}}
 #'  using [`<tidy-select>` arguments](https://dplyr.tidyverse.org/reference/dplyr_tidy_select.html); 
-#'  - Subset using indices with `slice`;
-#'  - Order genomic interactions with `arrange` using categorical/numerical 
+#'  - Subset using indices with \code{\link[=dplyr-slice]{slice}};
+#'  - Order genomic interactions with \code{\link[=dplyr-arrange]{arrange}} using categorical/numerical 
 #'  variables. 
 #'
 #'    For more details on the features of plyinteractions, read the vignette:
